0b5d994a8ef1ca875d391e6180428c1268c64a2d
swamfire <usern5949@gmail.com>
2024-09-10 23:40:34 +0530

    Added status codes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query, Path, Body
+from fastapi import FastAPI, Query, Path, Body, status
 from enum import Enum
 from typing import Optional, List, Set, Dict, Union, Literal
 from pydantic import BaseModel, Field, HttpUrl, EmailStr
@@ -242,89 +242,104 @@ app = FastAPI()
 #     return items[item_id]
 
 
-# Extra Models
+# # Extra Models
+# class UserBase(BaseModel):
+#     username: str
+#     email: EmailStr
+#     full_name: str or None = None
 
 
-class UserBase(BaseModel):
-    username: str
-    email: EmailStr
-    full_name: str or None = None
+# class UserIn(UserBase):
+#     password: str
 
 
-class UserIn(UserBase):
-    password: str
+# class UserOut(UserBase):
+#     pass
 
 
-class UserOut(UserBase):
-    pass
+# class UserInDB(UserBase):
+#     hashed_password: str
 
 
-class UserInDB(UserBase):
-    hashed_password: str
+# def fake_password_hasher(raw_password: str):
+#     return f"supersecret{raw_password}"
 
 
-def fake_password_hasher(raw_password: str):
-    return f"supersecret{raw_password}"
+# def fake_save_user(user_in: UserIn):
+#     hashed_password = fake_password_hasher(user_in.password)
+#     user_in_db = UserInDB(**user_in.dict(), hashed_password=hashed_password)
+#     print("User 'saved'.")
+#     return user_in_db
 
 
-def fake_save_user(user_in: UserIn):
-    hashed_password = fake_password_hasher(user_in.password)
-    user_in_db = UserInDB(**user_in.dict(), hashed_password=hashed_password)
-    print("User 'saved'.")
-    return user_in_db
+# @app.post("/user/", response_model=UserOut)
+# async def create_user(user_in: UserIn):
+#     user_saved = fake_save_user(user_in)
+#     return user_saved
 
 
-@app.post("/user/", response_model=UserOut)
-async def create_user(user_in: UserIn):
-    user_saved = fake_save_user(user_in)
-    return user_saved
+# class BaseItem(BaseModel):
+#     description: str
+#     type: str
 
 
-class BaseItem(BaseModel):
-    description: str
-    type: str
+# class CarItem(BaseItem):
+#     type: str or "car"
 
 
-class CarItem(BaseItem):
-    type: str or "car"
+# class PlaneItem(BaseItem):
+#     type: str or "plane"
+#     size: int
 
 
-class PlaneItem(BaseItem):
-    type: str or "plane"
-    size: int
+# items = {
+#     "item1": {"description": "All my friends drive a low rider", "type": "car"},
+#     "item2": {
+#         "description": "Music is my aeroplane, it's my aeroplane",
+#         "type": "plane",
+#         "size": 5,
+#     },
+# }
 
 
-items = {
-    "item1": {"description": "All my friends drive a low rider", "type": "car"},
-    "item2": {
-        "description": "Music is my aeroplane, it's my aeroplane",
-        "type": "plane",
-        "size": 5,
-    },
-}
+# @app.get("/items/{item_id}", response_model=Union[PlaneItem, CarItem])
+# async def read_item(item_id: Literal["item1", "item2"]):
+#     return items[item_id]
 
 
-@app.get("/items/{item_id}", response_model=Union[PlaneItem, CarItem])
-async def read_item(item_id: Literal["item1", "item2"]):
-    return items[item_id]
+# class ListItem(BaseModel):
+#     name: str
+#     description: str
 
 
-class ListItem(BaseModel):
-    name: str
-    description: str
+# list_items = [
+#     {"name": "Foo", "description": "There comes my hero"},
+#     {"name": "Red", "description": "It's my aeroplane"},
+# ]
 
 
-list_items = [
-    {"name": "Foo", "description": "There comes my hero"},
-    {"name": "Red", "description": "It's my aeroplane"},
-]
+# @app.get("/list_items/", response_model=List[ListItem])
+# async def read_items():
+#     return list_items
 
 
-@app.get("/list_items/", response_model=List[ListItem])
-async def read_items():
-    return list_items
+# @app.get("/arbitrary", response_model=Dict[str, float])
+# async def get_arbitrary():
+#     return {"foo": 1, "bar": "2"}
 
 
-@app.get("/arbitrary", response_model=Dict[str, float])
-async def get_arbitrary():
-    return {"foo": 1, "bar": "2"}
+# Response status codes
+@app.post("/items/", status_code=status.HTTP_201_CREATED)
+async def create_item(name: str):
+    return {"name": name}
+
+
+@app.delete("/items/{pk}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_item(pk: str):
+    print("pk", pk)
+    return pk
+
+
+@app.get("/items/", status_code=status.HTTP_302_FOUND)
+async def read_items_redirect():
+    return {"hello": "world"}
