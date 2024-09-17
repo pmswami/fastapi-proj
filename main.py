0cf886e81f18dee80dfb172f5d9629b9ea08d1ac
swamfire <usern5949@gmail.com>
2024-09-18 00:45:30 +0530

    Added class based  dependencies

--- main.py
+++ main.py
@@ -619,22 +619,43 @@ app = FastAPI()
 #     return updated_item
 
 
-# Dependencies
-async def hello():
-    return "world"
+# # function based Dependencies
+# async def hello():
+#     return "world"
 
 
-async def common_parameters(
-    q: str or None = None, skip: int = 0, limit: int = 100, blah: str = Depends(hello)
-):
-    return {"q": q, "skip": skip, "limit": limit, "hello": blah}
+# async def common_parameters(
+#     q: str or None = None, skip: int = 0, limit: int = 100, blah: str = Depends(hello)
+# ):
+#     return {"q": q, "skip": skip, "limit": limit, "hello": blah}
 
 
-@app.get("/items/")
-async def read_items(commons: dict = Depends(common_parameters)):
-    return commons
+# @app.get("/items/")
+# async def read_items(commons: dict = Depends(common_parameters)):
+#     return commons
 
 
-@app.get("/users/")
-async def read_users(commons: dict = Depends(common_parameters)):
-    return commons
+# @app.get("/users/")
+# async def read_users(commons: dict = Depends(common_parameters)):
+#     return commons
+
+
+# class based dependencies
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+class CommonQueryParams:
+    def __init__(self, q: str or None = None, skip: int = 0, limit: int = 100):
+        self.q = q
+        self.skip = skip
+        self.limit = limit
+
+
+@app.get("/items/{item_id}")
+async def read_items(commons: CommonQueryParams = Depends()):
+    response = {}
+    if commons.q:
+        response.update({"q": commons.q})
+    items = fake_items_db[commons.skip : commons.skip + commons.limit]
+    response.update({"items": items})
+    return response
