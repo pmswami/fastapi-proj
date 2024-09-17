6d5f1d4f14c2d80fcccb08e034c8c0722ea8b743
swamfire <usern5949@gmail.com>
2024-09-17 13:16:44 +0530

    Added functional dependencies

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from fastapi import (
     UploadFile,
     HTTPException,
     Request,
+    Depends,
 )
 from fastapi.exception_handlers import (
     http_exception_handler,
@@ -567,52 +568,73 @@ app = FastAPI()
 #     return [{"item_id": "Foo"}]
 
 
-class Item(BaseModel):
-    name: str or None = None
-    description: str or None = None
-    price: float or None = None
-    tax: float = 10.5 or 0
-    tags: List[str] = []
+# class Item(BaseModel):
+#     name: str or None = None
+#     description: str or None = None
+#     price: float or None = None
+#     tax: float = 10.5 or 0
+#     tags: List[str] = []
 
 
-items = {
-    "foo": {"name": "Foo", "price": 50.2},
-    "bar": {
-        "name": "Bar",
-        "description": "The bartenders",
-        "price": 62,
-        "tax": 20.2,
-    },
-    "baz": {
-        "name": "Baz",
-        "description": None,
-        "price": 50.2,
-        "tax": 10.5,
-        "tags": [],
-    },
-}
+# items = {
+#     "foo": {"name": "Foo", "price": 50.2},
+#     "bar": {
+#         "name": "Bar",
+#         "description": "The bartenders",
+#         "price": 62,
+#         "tax": 20.2,
+#     },
+#     "baz": {
+#         "name": "Baz",
+#         "description": None,
+#         "price": 50.2,
+#         "tax": 10.5,
+#         "tags": [],
+#     },
+# }
 
 
-@app.get("/items/{item_id}", response_model=Item)
-async def read_item(item_id: str):
-    return items.get(item_id)
+# @app.get("/items/{item_id}", response_model=Item)
+# async def read_item(item_id: str):
+#     return items.get(item_id)
 
 
-@app.put("/items/{item_id}", response_model=Item)
-def update_item(item_id: str, item: Item):
-    update_item_encoded = jsonable_encoder(item)
-    items[item_id] = update_item_encoded
-    return update_item_encoded
+# @app.put("/items/{item_id}", response_model=Item)
+# def update_item(item_id: str, item: Item):
+#     update_item_encoded = jsonable_encoder(item)
+#     items[item_id] = update_item_encoded
+#     return update_item_encoded
 
 
-@app.patch("/items/{item_id}", response_model=Item)
-def patch_item(item_id: str, item: Item):
-    stored_item_data = items.get(item_id)
-    if stored_item_data is not None:
-        stored_item_model = Item(**stored_item_data)
-    else:
-        stored_item_model = Item()
-    update_data = item.dict(exclude_unset=True)
-    updated_item = stored_item_model.copy(update=update_data)
-    items[item_id] = jsonable_encoder(updated_item)
-    return updated_item
+# @app.patch("/items/{item_id}", response_model=Item)
+# def patch_item(item_id: str, item: Item):
+#     stored_item_data = items.get(item_id)
+#     if stored_item_data is not None:
+#         stored_item_model = Item(**stored_item_data)
+#     else:
+#         stored_item_model = Item()
+#     update_data = item.dict(exclude_unset=True)
+#     updated_item = stored_item_model.copy(update=update_data)
+#     items[item_id] = jsonable_encoder(updated_item)
+#     return updated_item
+
+
+# Dependencies
+async def hello():
+    return "world"
+
+
+async def common_parameters(
+    q: str or None = None, skip: int = 0, limit: int = 100, blah: str = Depends(hello)
+):
+    return {"q": q, "skip": skip, "limit": limit, "hello": blah}
+
+
+@app.get("/items/")
+async def read_items(commons: dict = Depends(common_parameters)):
+    return commons
+
+
+@app.get("/users/")
+async def read_users(commons: dict = Depends(common_parameters)):
+    return commons
